653ef62b32a384f485a9f6de399bf7739f4e2768
lenin <lenin@cybrilla.com>
2019-12-10 12:39:21 +0530

    Fix priavte key usage issue

--- authentication/hmac-postman-pre-request-script.js
+++ authentication/hmac-postman-pre-request-script.js
@@ -1,28 +1,33 @@
+/**
+ *  https://fintechprimitives.com/api/#hmac-token-generation
+ *  
+ *  This is mainly used as a pre-request-script in postman and uses postman sandbox apis and postman sandbox supported 
+ *  node modules.
+ * 
+ *  This script will not work as such in node js or javascript environment, without modifications of this script
+ * 
+ */
 const crypto = require('crypto-js');
 const url = pm.request.url.getPath();
-// tenant name provided
+// Tenant name shared with the client
 const tenant="<tenant_name>";
-// secret key provided
-const encryptkey = "<secret_key>";
-const apikey = tenant;
 
-const env = pm.environment.get("environment");
-let key = `${env}-secret`;
+// Private api key shared with the client
+const privatekey = "<private api key>";
 
-console.log(`Env = ${pm.environment.get(key)}`);
-
+// This date will be in this format `20150830T123600Z`. Check the api doc for more details
 const requesttime = new Date().toISOString().replace(/[-:]/g, '').replace(/\.{1}\d*/g, '');
 
 const body = hmacBody();
 console.log("Body = ", body);
-const signedKey = signBody(apikey, body);
+const signedKey = signBody(privatekey, body);
 const time = new Date().toISOString().replace(/-/g, '').replace(/T.*Z/g, '');
 const signedDate = signBody(signedKey, time);
 const signedTenant = signBody(signedDate, tenant);
-const hmacToken = signBody(signedTenant, encryptkey);
+const hmacToken = signBody(signedTenant, 'mfprocybrilla');
 
-
-console.log("Hmac Token = ", hmacToken);
+console.log("Hmac Token = ", hmacToken);  
+console.log(requesttime)
 
 // Add Basic Header
 pm.request.headers.add({
@@ -40,6 +45,8 @@ pm.request.headers.add({
     value: 'application/json'
 });
 
+// console.log(pm.request.headers)
+
 function signBody(key, body) {
     return crypto.enc.Base64.stringify(crypto.HmacSHA256(body, key));
 }
@@ -47,6 +54,7 @@ function signBody(key, body) {
 function hmacBody(){
     const time = requesttime;
     const requestMethod = 'POST';
+    console.log(crypto.MD5)
     const digest = crypto.MD5("").toString();
     const contentType = 'application/json';
     return (requestMethod + "\n" + digest + "\n" + contentType + "\n" +
