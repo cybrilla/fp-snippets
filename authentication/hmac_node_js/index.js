7f5afaf62db0cbbe32015194fc8fb7e47b7e318c
Madhavan <maddy.srv@gmail.com>
2021-06-25 16:57:11 +0530

    Add Api Key ID

--- authentication/hmac_node_js/index.js
+++ authentication/hmac_node_js/index.js
@@ -6,7 +6,8 @@ const request = require('request')
 const tenant = argv['tenant']
 
 //Private Key from args
-const private_key = argv['secret']
+const apiKeySecret = argv['apiKeySecret']
+const apiKeyId = argv['apiKeyId']
 
 //Get domain name
 const hostName = argv['domain'] || 'https://api.fintechprimitives.com'
@@ -16,7 +17,7 @@ const requesttime = new Date().toISOString().replace(/[-:]/g, '').replace(/\.{1}
 
 
 const body = hmacBody();
-const signedKey = signBody(private_key, body);
+const signedKey = signBody(apiKeySecret, body);
 const time = new Date().toISOString().replace(/-/g, '').replace(/T.*Z/g, '');
 const signedDate = signBody(signedKey, time);
 const signedTenant = signBody(signedDate, tenant);
@@ -25,7 +26,7 @@ const hmacToken = signBody(signedTenant, 'mfprocybrilla');
 
 const httpHeaders = { 
     'content-type': 'application/json',
-    'Authorization': tenant+":"+hmacToken,
+    'Authorization': apiKeyId+":"+hmacToken,
     'MFProDate': requesttime,
     'x-tenant-id': tenant
 }
@@ -35,21 +36,21 @@ const httpOptions = {
     headers: httpHeaders,
     method: 'POST'
 }
-request(httpOptions, function(err, res, body) {
-    let json = JSON.parse(body);
+request(httpOptions, function(err, res, requestBody) {
+    let json = JSON.parse(requestBody);
     console.log(json);
 })
 
-function signBody(key, body) {
-    return crypto.enc.Base64.stringify(crypto.HmacSHA256(body, key));
+function signBody(key, requestBody) {
+    return crypto.enc.Base64.stringify(crypto.HmacSHA256(requestBody, key));
 }
 
 //Build hmac body
 function hmacBody(){
-    const time = requesttime;
+    const currentTime = requesttime;
     const requestMethod = 'POST';
     const digest = crypto.MD5("").toString();
     const contentType = 'application/json';
     return (requestMethod + "\n" + digest + "\n" + contentType + "\n" +
-           time + "\n" + path);
+        currentTime + "\n" + path);
 }
